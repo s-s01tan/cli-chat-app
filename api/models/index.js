c9909182d5db383efd6cd3904377e3b8c425fa34
sssoltannn <soltanlisoltan.js@gmail.com>
2022-07-15 18:20:38 +0400

    Moved to API folder

---
+++ api/models/index.js
@@ -0,0 +1,26 @@
+const { connect, Schema, model } = require('mongoose')
+const { db_url } = require('../../config')
+const { generateUserSchema, generateMessageSchema, generateRoomSchema } = require('../schemas')
+require('colors')
+
+// Database Connection
+connect(db_url, (err) => {
+    if (err) return console.log(`${'•'.red} Failed to connect Database;`)
+    else console.log(`${'•'.green} Connected Database Successfully;`)
+})
+
+// Schemas
+const userSchema = generateUserSchema(Schema)
+const roomSchema = generateMessageSchema(Schema)
+const messageSchema = generateRoomSchema(Schema)
+
+// Models
+const User = model('User', userSchema)
+const Message = model('Message', messageSchema)
+const Room = model('Room', roomSchema)
+
+module.exports = {
+    User,
+    Message,
+    Room
+}
