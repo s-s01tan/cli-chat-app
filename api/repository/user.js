d85ca69454d9d2cd1de081547b2cf0efc4fa6c47
sssoltannn <soltanlisoltan.js@gmail.com>
2022-07-15 19:01:27 +0400

    Searching and Finding users by Username or Fullname

--- api/repository/user.js
+++ api/repository/user.js
@@ -11,6 +11,33 @@ class UserRepository {
             console.log('Database Error: '.red, e)
         }
     }
+
+    // Searchs user by given username
+    async searchByUsername(username) {
+        try {
+            return await User.findOne({ username: { $regex: username, $options: 'i' } })
+        } catch (e) {
+            console.log('Database Error: '.red, e)
+        }
+    }
+
+    // Finds user by given fullname
+    async findByFullname(fullName) {
+        try {
+            return await User.findOne({ fullName })
+        } catch (e) {
+            console.log('Database Error: '.red, e)
+        }
+    }
+
+    // Searchs user by given fullname
+    async searchByFullname(fullName) {
+        try {
+            return await User.findOne({ fullName: { $regex: fullName, $options: 'i' } })
+        } catch (e) {
+            console.log('Database Error: '.red, e)
+        }
+    }
 }
 
 module.exports = UserRepository
