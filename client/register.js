7866cc8beae2653d5f30803631cf24eb8a62554f
sssoltannn <soltanlisoltan.js@gmail.com>
2022-07-15 17:51:24 +0400

    Registration Validations

---
+++ client/register.js
@@ -0,0 +1,45 @@
+// User Registration
+const registerUser = (inquirer) => {
+    let passTmp = ''
+    inquirer.prompt([
+        {
+            name: 'fullName',
+            message: 'Full Name: '
+        },
+        {
+            name: 'username',
+            message: 'Username: '
+        },
+        {
+            name: 'password',
+            message: 'Password: ',
+            type: 'password',
+            validate: function (input) {
+                const done = this.async()
+                if (input.length < 6) {
+                    done('Passwords must be at least 6 characters long;')
+                    return
+                }
+                passTmp = input
+                done(null, true)
+            }
+        },
+        {
+            name: 're_password',
+            message: 'Confirm Password: ',
+            type: 'password',
+            validate: function (input) {
+                const done = this.async()
+                if (input !== passTmp) {
+                    done('Your password and confirmation password do not match;')
+                    return
+                }
+                done(null, true)
+            }
+        }
+    ]).then(answers => {
+        console.log(answers)
+    })
+}
+
+module.exports = registerUser
