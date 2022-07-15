4d1a6f5ced3ba8836ad3f8bded2591ae08fb2ef0
sssoltannn <soltanlisoltan.js@gmail.com>
2022-07-15 19:02:28 +0400

    Applied Routers

--- server.js
+++ server.js
@@ -8,12 +8,23 @@ const server = createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server)
 
-const { config } = require('dotenv')
-const { welcomeWave, appInfo } = require('./art')
-config()
+// Middlewares
+app.use(express.urlencoded())
+app.use(express.json())
+
+// Routers
+const { userRouter, messageRouter, roomRouter } = require('./api/routers')
+app.use('/user', userRouter)
+app.use('/message', messageRouter)
+app.use('/room', roomRouter)
+
+// Request Logger
+const morgan = require('morgan')
+app.use(morgan('tiny'))
 
 // Listening event
-const { host, port } = require('./config')(process.env.NODE_ENV)
+const { host, port } = require('./config')()
+const { welcomeWave, appInfo } = require('./art')
 server.listen(port, async () => {
     console.log(welcomeWave())
     console.log(appInfo(host, port))
