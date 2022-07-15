f6f44e176b0a75cc19efda708763a41b9f44ccbb
sssoltannn <soltanlisoltan.js@gmail.com>
2022-07-15 19:02:14 +0400

    Got Env from Config File

--- config.js
+++ config.js
@@ -1,25 +1,27 @@
-module.exports = (env) => {
+require('dotenv')
+const node_env = process.env.NODE_ENV || 'development'
+
+module.exports = () => {
     const config = {
         test: {
             host: 'http://localhost',
             port: 3000,
             db_url: 'mongodb://localhost:27017/cli_chat_db',
-            basic_url: 'http://localhost:3000'
+            base_url: 'http://localhost:3000'
         },
         development: {
             host: 'http://localhost',
             port: 3000,
             db_url: 'mongodb://localhost:27017/cli_chat_db',
-            basic_url: 'http://localhost:3000'
+            base_url: 'http://localhost:3000'
         },
         production: {
             host: 'http://localhost',
             port: 3000,
             db_url: 'mongodb://localhost:27017/cli_chat_db',
-            basic_url: 'http://localhost:3000'
+            base_url: 'http://localhost:3000'
         }
     }
 
-    // return Object.keys(config).includes(env) ? config[env] : config['development']
-    return env === 'test' || env === 'development' || env === 'production' ? config[env] : config['development']
+    return config[node_env]
 }
