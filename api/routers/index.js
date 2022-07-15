9656f921c047007bc27d7bb3529b7cda0a081516
sssoltannn <soltanlisoltan.js@gmail.com>
2022-07-15 19:01:33 +0400

    Exports all routers

---
+++ api/routers/index.js
@@ -0,0 +1,9 @@
+const userRouter = require('./user')
+const messageRouter = require('./message')
+const roomRouter = require('./room')
+
+module.exports = {
+    userRouter,
+    messageRouter,
+    roomRouter
+}
