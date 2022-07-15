65f69d1072fc32e570a2d97ce5efef725e994af5
sssoltannn <soltanlisoltan.js@gmail.com>
2022-07-15 19:02:06 +0400

    Validated Exist Username

--- client/register.js
+++ client/register.js
@@ -1,14 +1,44 @@
+const axios = require('axios')
+const { base_url } = require('../config')()
+
+
 // User Registration
 const registerUser = async (inquirer) => {
     let passTmp = ''
     inquirer.prompt([
         {
             name: 'fullName',
-            message: 'Full Name: '
+            message: 'Full Name: ',
+            validate: function (input) {
+                const done = this.async()
+                if (input.length < 3) {
+                    done('Fullname must be at least 3 characters long;')
+                    return
+                }
+                done(null, true)
+            }
         },
         {
             name: 'username',
-            message: 'Username: '
+            message: 'Username: ',
+            validate: function (input) {
+                const done = this.async()
+
+                if (input.length < 3) {
+                    done('Username must be at least 3 characters long;')
+                    return
+                }
+
+                axios.post(`${base_url}/user/check?by=username`, {
+                    value: input
+                }).then(response => {
+                    if (response.hasFound) {
+                        done('Username has already been taken;')
+                        return
+                    }
+                    done(null, true)
+                })
+            }
         },
         {
             name: 'password',
